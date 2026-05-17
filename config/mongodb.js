1f2b6676ce3dcb6daf1833679fea725aec71d2b4
Jubril <jubril914@gmail.com>
2026-05-17 13:22:45 +0100

    mnjjbb

--- config/mongodb.js
+++ config/mongodb.js
@@ -9,8 +9,10 @@ const connectDB = async () => {
     mongoose.connection.on("connected", () => {
         console.log("MongoDB connected successfully");
     });
+
+    await mongoose.connect(`${process.env.MONGODB_URI}login`)
 }
 
-await mongoose.connect(`${process.env.MONGODB_URI}login`)
+
 
 export default connectDB;   
